93cdfd8085577a18ac3fc9e9ec8b7d3fc7ce812c
islam-fullstack <islam.qiyasov0823@gmail.com>
2021-07-24 23:14:50 +0500

    js api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //b6ae94889c6283d60a177e62fbb4debd
+const api = {
+    key: 'b6ae94889c6283d60a177e62fbb4debd',
+    baseUrl: 'api.openweathermap.org/data/2.5/'
+}
+
+const search = document.querySelector('.search');
+
+search.addEventListener('keypress', setQuery());
+
+function setQuery() {
+
+}
